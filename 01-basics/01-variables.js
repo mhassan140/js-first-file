0d20e8735513d4e20a7eb8f0e0b6606688db7eca
mhassan140 <123256770+mhassan140@users.noreply.github.com>
2023-09-06 15:57:36 +0000

    my work

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "mohsin@google.com"
+var accountPassword ="12345"
+accountCity = "karachi"
+let accountState;
+
+// accountId = 2 not allowed
+/*
+now modern day programing is use let and const not var bcz of
+block scope and functional scope
+*/
+// console.log(accountId);
+accountEmail = "hassan@goole.com"
+accountPassword = "1222122"
+accountCity = "lahore"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
--- 01-basics/test.js
+++ 01-basics/test.js
@@ -1,1 +1,1 @@
-console.log("mohsin")
+console.log("Mohsin")

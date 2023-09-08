0b971c04ece7f6e5b7e5dde5e3e67439d4281aa5
mhassan140 <123256770+mhassan140@users.noreply.github.com>
2023-09-08 14:37:41 +0000

    array

--- 01-basics/01-variables.js
+++ 01-basics/01-variables.js
@@ -159,6 +159,19 @@ block scope and functional scope
 
 // let mycreatedDate = new Date(2023, 0, 23)
 // console.log(mycreatedDate.toDateString())
+// let myTimeStamp = Date.now()
+// console.log (Math.floor(Date.now()/1000));
+
+// let newDate = new Date()
+// console.log (newDate);
+// console.log (newDate.getMonth() + 1);
+// console.log (newDate.getDay());
+
+// newDate.toLocaleString('default', {
+//     weekday: "long"
+// })
+// console.log (newDate);
+
 
 
 
--- 01-basics/02-arrays.js
+++ 01-basics/02-arrays.js
@@ -1,9 +1,9 @@
 // ***************arrays***************
-const myArr = [0, 1, 2, 3, 4, 5]
+// const myArr = [0, 1, 2, 3, 4, 5]
 // const myCities = ["islamabad", "karachi"]
 
 // const myArr2 = new Array (1, 2, 3,)
-console.log (myArr);
+// console.log (myArr);
 
 // array methods 
 
@@ -13,3 +13,38 @@ console.log (myArr);
 // myArr.shift() .shift arase first value of the array
 // console.log (myArr.includes(9)); this method is checked 9 is available in your array and ans in boolean
 // console.log (myArr.indexOf(3)); this method cheked index of the value 
+// const newArr = myArr.join
+// console.log (myArr);
+// console.log (typeof newArr);
+
+// const myn1 = myArr.slice(1, 3)
+// console.log(myn1);
+// console.log("B ", myArr);
+
+// const myn2 = myArr.splice(1, 3)
+// console.log (myn2);
+
+// veg = ["carrot", "cucumber", "spinich"]
+// fruit = ["mango", "apple" , "orange"]
+
+// veg.push (fruit)
+// console.log (veg);
+
+// const  allFruits = (veg .concat (fruit)) this aray method combine two aray and give us 3rd aray combine
+// console.log (allFruits);
+// const allFruit = [... fruit, ... veg] this method also a same as concat spread method 
+// console.log(allFruit);
+
+// const another_Array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+// const real_another_Array = another_Array.flat(Infinity)
+// console.log (real_another_Array);
+
+// console.log (Array.isArray("mohsin"));
+// console.log (Array.from("hassan"));
+// console.log (Array.from({name:"shahid"})); intrested 
+
+// let score1 = 100 
+// let score2 = 200
+// let score3= 300
+// console.log (Array.of(score1,score2,score3)); some time you have multiple values this method convert all in array
+

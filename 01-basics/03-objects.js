e72453ce02306617d88e387fa183dd3fb8febae8
mhassan140 <123256770+mhassan140@users.noreply.github.com>
2023-09-08 15:52:53 +0000

    objects

---
+++ 01-basics/03-objects.js
@@ -0,0 +1,30 @@
+// ****************(objects)***************
+// const fullName = "HASSAN"
+// const juser = {
+//     name : "mohsin",
+//     fullName : ["hassan"],
+//     age : 30,
+//     email : "mohsinhassan@google.com",
+//     city : "karachi",
+//     isLoggedIn : false,
+//     lastLoggedIn : ["monday", "saturday"]
+// }
+// console.log (juser.email);
+// console.log (juser["email"]);
+// console.log (juser["fullName"]);
+// console.log (juser);
+
+
+// juser.email = "mohsinhassan@chatgpt.com"
+// // Object.freeze(juser)
+// juser.email = "hassan123@google.com"
+// console.log (juser);
+
+// juser.greeting = function(){
+//     console.log ("hello js user");
+// }
+//     juser.greetingTwo = function(){
+//         console.log (`hello js user${this.name}`);
+// }
+// console.log(juser.greetingTwo());
+// console.log(juser.greeting());

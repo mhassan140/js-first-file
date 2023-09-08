3c977d09458c4f43c4e5c52f4835caa962d567c1
mhassan140 <123256770+mhassan140@users.noreply.github.com>
2023-09-08 11:58:08 +0000

    array

---
+++ 01-basics/02-arrays.js
@@ -0,0 +1,15 @@
+// ***************arrays***************
+const myArr = [0, 1, 2, 3, 4, 5]
+// const myCities = ["islamabad", "karachi"]
+
+// const myArr2 = new Array (1, 2, 3,)
+console.log (myArr);
+
+// array methods 
+
+// myArr.push (6) .push method simply add some value in array.
+// myArr.pop() .pop simply erase the last value of array
+// myArr.unshift(9) .unshift simply add some value in start of the array
+// myArr.shift() .shift arase first value of the array
+// console.log (myArr.includes(9)); this method is checked 9 is available in your array and ans in boolean
+// console.log (myArr.indexOf(3)); this method cheked index of the value 

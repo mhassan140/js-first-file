22c7f6d9e9b1a7ede7af542fa65fd8b9a0eb6bac
mhassan140 <123256770+mhassan140@users.noreply.github.com>
2023-09-13 15:18:48 +0000

    work

---
+++ 01-basics/05-scopes.js
@@ -0,0 +1,20 @@
+// var a = 200
+// if(true){
+//     var a = 10
+//     const b = 20
+//     let c =30
+//     console.log("INNER:",a);
+// }
+// console.log(a)
+
+// nested scopes
+
+// if(true){
+// const username = "mohsin-hassan"
+// if(username === "mohsin-hassan"){
+// const website = " youtube"
+// console.log(username + website);
+// }
+    // console.log(website);
+// }
+// console.log(username);
---
+++ 01-basics/06-arrow-function.js
@@ -0,0 +1,11 @@
+const user = {
+    username: "mohsin",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log(`${this.username},welcome to website`);
+    }
+}
+    user.welcomeMessage()
+    user.username = "hassan"
+    user.welcomeMessage

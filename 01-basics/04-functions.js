1ab71e35533398e1e6bb2830d68b24efbd323326
mhassan140 <123256770+mhassan140@users.noreply.github.com>
2023-09-13 11:35:19 +0000

    function

---
+++ 01-basics/04-functions.js
@@ -0,0 +1,24 @@
+// function sayMyName(){
+//     console.log ("m");
+//     console.log ("o");
+//     console.log ("h");
+//     console.log ("s");
+//     console.log ("i");
+//     console.log ("n");
+// }
+// sayMyName()
+
+// function addtwoNumbers(number1,number2){
+//     console.log (number1 + number2);
+// }
+// addtwoNumbers(5,5)
+
+function addtwoNumbers(number1,number2){
+    // let result = number1+number2 this is first syntex
+//  return result
+// return number1+number2    this is second syntax
+
+
+}
+const result = addtwoNumbers (5,5)
+console.log ("Result: ", result);

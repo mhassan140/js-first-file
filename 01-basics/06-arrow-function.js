4b0333421d8e3f38bc2991bb3bff62bd535644a6
mhassan140 <123256770+mhassan140@users.noreply.github.com>
2023-09-13 15:45:32 +0000

    arrow

--- 01-basics/06-arrow-function.js
+++ 01-basics/06-arrow-function.js
@@ -1,11 +1,18 @@
-const user = {
-    username: "mohsin",
-    price: 999,
+// const user = {
+//     username: "mohsin",
+//     price: 999,
 
-    welcomeMessage: function(){
-        console.log(`${this.username},welcome to website`);
+//     welcomeMessage: function(){
+//         console.log(`${this.username},welcome to website`);
+//         console.log (this);
+//     }
+// }
+    // user.welcomeMessage()
+    // user.username = "hassan"
+    // user.welcomeMessage()
+    // console.log(this);
+
+    const addTwo = (num1,num2) =>{
+return num1 + num2
     }
-}
-    user.welcomeMessage()
-    user.username = "hassan"
-    user.welcomeMessage
+    console.log(addTwo(3 , 4));

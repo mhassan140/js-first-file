8809b8cac9566ae9ecfb30fb139f076673adf9d9
mhassan140 <123256770+mhassan140@users.noreply.github.com>
2023-09-20 17:27:33 +0000

    loop

--- 01-basics/07-loops.js
+++ 01-basics/07-loops.js
@@ -75,3 +75,118 @@
 //     console.log(`score is ${score}`);
 //     score++
 // } while (score <= 10);
+
+
+// for of loop
+
+// const arr = [1, 2, 3, 4, 5]
+// for (const num of arr) {
+//     console.log(num);
+    
+// // }
+
+
+// const greeting = "hello how are you"
+// for (const greet of greeting) {
+//     console.log(`each char is ${greet}`)
+    
+// }
+
+
+// maps
+
+// const map = new Map()
+// map.set ('PAK', "pakistan")
+// map.set ('USA', "united cstates of america")
+// map.set ('FR', "france")
+// // console.log(map);
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+    
+// }
+// for in loop
+// const myObject = {
+// js: "java script",
+// cpp: "c++",
+// rb: "ruby",
+// swift: "swift by apple"
+// }
+// for (const key in myObject) {
+// //    console.log(key);
+// //     console.log(myObject[key]);  
+//     console.log(`${key} shortcut is for ${myObject[key]}`);  
+//     }
+
+// const programing = ['js', 'java', 'ruby', 'py', 'swift']
+// for (const key in programing) {
+//    console.log(programing[key]);
+    
+// }
+
+// for each loop
+
+
+// const coding = ['js', 'java', 'ruby', 'swift',]
+// coding.forEach( function(item,index,arr){     this is call back function
+// // coding.forEach ( (item)=>{}) this is arrow function
+
+// // } )
+// console.log(item,index,arr);
+// })
+
+// const myCodding = [
+//     {
+//         languageName: "javaScript",
+//         languageFileName: "js"
+//     },
+//     {
+//         languageName: "python",
+//         languageFileName: "py"
+//     },
+//     {
+//         languageName: "java",
+//         languageFileName: "java"
+//     },
+// ]
+// myCodding.forEach(function (item){
+//     console.log(item.languageName);
+
+// })
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNums = myNums.filter( (num)=>num >5)
+// console.log(newNums);
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNums = []
+// myNums.forEach( (num)=>{
+//     if (num >4) {
+//         newNums.push(num)
+//     }
+
+// })
+// console.log(newNums);
+
+// const myNums =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNums = myNums.map( (num)=> num  + 10)
+// console.log(newNums);
+
+
+// chaining
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNums = myNums
+//                  .map( (num)=> num * 10)
+//                  .map( (num)=> num + 1)
+//                  .filter( (num) => num >= 40) .filter condition depends on true /false 
+//                  console.log(newNums);
+
+// const myNums = [1, 2, 3]
+// const myTotal = myNums.reduce(function(acc , currval){
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+// return acc + currval
+// }, 0)
+// console.log(myTotal);
+
+// const myNums = [1, 2, 3]
+// const myTotal = myNums.reduce( (acc,currval)=>acc+currval,0)
+// console.log(myTotal);
